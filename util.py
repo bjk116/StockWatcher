2cd9e228ed990eedf9eb067696d34bf0cb5de99b
Brian <karabinchak.brian@gmail.com>
2021-11-20 19:08:26 -0500

    Started util functions

---
+++ util.py
@@ -0,0 +1,10 @@
+"""
+FUnctions used all over.  Mostly time related.
+Shit should I call this something time related instaed?
+"""
+
+def default_start_end_times(start_date=None, end_date=None):
+    """
+
+    """
+    pass
