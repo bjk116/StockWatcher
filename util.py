a78b8de6e08a456a29874e878ead0e6a65d6ccfb
Brian <karabinchak.brian@gmail.com>
2021-11-26 18:18:27 -0500

    Function to make list of datetimes with variable interval length

--- util.py
+++ util.py
@@ -2,9 +2,26 @@
 FUnctions used all over.  Mostly time related.
 Shit should I call this something time related instaed?
 """
+from datetime import datetime, timedelta
 
 def default_start_end_times(start_date=None, end_date=None):
     """
 
     """
     pass
+
+def get_intervals(start_date, end_date, **kwargs):
+    """
+    Given two dates, get list of intervals between them.
+    Args:
+        start_date: datetime, oldest date to being
+        end_date: datetime, lats interval
+        **kwargs: info about what sort of interval to put into effect, look up datetime.timedelta doc for more info
+    Returns:
+        list of datetimes
+    """    
+    invervals = []
+    while start_date <= end_date:
+        invervals.append(start_date)
+        start_date = start_date + timedelta(**kwargs)
+    return invervals
