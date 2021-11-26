2e23e04b4d11e26efdc2d31cf08e209333f77a04
Brian <karabinchak.brian@gmail.com>
2021-11-26 18:14:45 -0500

    First attempt class that represents a single record

--- classes.py
+++ classes.py
@@ -1,6 +1,8 @@
 """
 For classes related to database and general concerns.
 """
+import db
+
 class Dataset():
     data = None
     columns = None
@@ -19,3 +21,24 @@ class Dataset():
     
     def __getitem__(self):
         return self.data.__getitem__(self.data)
+
+class Record():
+    def __init__(self):
+        self.checkDBTablExistence()
+    
+    def checkDBTablExistence(self):
+        # TODO is this even necessary.  Calculating the db table name is, but checking existence is just for fun.
+        className = self.__class__.__name__
+        
+        className = f"{className.lower()}s"
+
+        query = f"SELECT COUNT(*) FROM information_schema.tables  WHERE table_schema = 'stock_watcher' AND table_name = '{className}';"
+        print(query)
+        result = db.runScalarQuery(query)
+        print("Result: ", str(result))
+
+class Notification(Record):
+    pass
+
+class Price(Record):
+    pass
